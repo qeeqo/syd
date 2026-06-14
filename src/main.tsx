6b837f4e53b27e491a7d5fd34d335977028b7954
qeeqo <177444912+qeeqo@users.noreply.github.com>
2026-06-14 10:21:19 +0800

    removed vite and reset, installed bun

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {createCliRenderer} from "@opentui/core";
+import {createRoot} from "@opentui/react";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const renderer = await createCliRenderer();
+createRoot(renderer).render(<App />);
